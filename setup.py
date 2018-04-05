4d9e261c0c2e0d6b10158a4289d6a5dc0b131aa7
jeffin143 <jeffinsam@karunya.edu.in>
2018-04-05 18:51:40 +0530

    testing

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  5 18:46:20 2018
+
+@author: jeffin
+"""
+
+from setuptools import setup
+
+setup(name='model',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/jeffin143/trainingm',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['model'],
+      zip_safe=False)
